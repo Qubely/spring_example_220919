8abaee3bd0f9267eb1ebadfe5617c22eeb80c6c4
test <test@test.com>
2022-12-28 20:59:04 +0900

    2022-12-28 Spring JSP ex02 과제

--- src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
@@ -1,16 +1,21 @@
 package com.example.lesson04;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.lesson04.bo.StudentBO;
 import com.example.lesson04.model.Student;
 
 @RequestMapping("/lesson04/ex02")
 @Controller
 public class Lesson04Ex02Controller {
+	
+	@Autowired
+	private StudentBO studentBO;
 	
 	// 학생 추가 화면
 	// http://localhost/lesson04/ex02/add_student_view
@@ -25,6 +30,8 @@ public class Lesson04Ex02Controller {
 			) {
 		
 		// DB insert
+		studentBO.addStudent(student);
+		
 		
 		// DB select(방금 가입된 사람)
 		
